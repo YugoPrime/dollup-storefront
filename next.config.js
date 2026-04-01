5e89261280395962cea4f9571a28dc0900deb9d6
root <root@srv1411338.hstgr.cloud>
2026-04-01 05:43:38 +0000

    feat: add cdn.dollupboutique.com to Next.js image domains

--- next.config.js
+++ next.config.js
@@ -42,6 +42,14 @@ const nextConfig = {
         protocol: "https",
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "cdn.dollupboutique.com",
+      },
+      {
+        protocol: "https",
+        hostname: "pub-da0a69100a054c009c778c5968b82070.r2.dev",
+      },
       ...(S3_HOSTNAME && S3_PATHNAME
         ? [
             {
